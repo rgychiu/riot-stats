ccca6acfcbdb90c2a56b29f86e61cb9e74e38a68
Roy Chiu <rychiu13@gmail.com>
2018-07-31 11:05:50 -0500

    Get top challenger player

--- core/Match_Harvester.py
+++ core/Match_Harvester.py
@@ -32,5 +32,19 @@ class MatchHarvester:
         """
         return self.make_request(self.CHALLENGER_PATH + self.config_manager.get_defaults_queue_id())
 
+    def get_top_challenger(self):
+        """
+        Get top challenger player based on number of wins.
+        :return: Player JSON data with highester amount of wins
+        """
+        challenger_list = self.get_sr_challenger_league()
+        max_wins = 0
+        player_json = None
+        for player in challenger_list:
+            if player['wins'] > max_wins:
+                max_wins = player['wins']
+                player_json = player
+        return player_json
+
     # TODO: Get match histories starting with the top challenger player
     # TODO: Get champion matchups from match histories

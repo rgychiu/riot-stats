4335a57e0147bfbc805d92152a0dc1bd77cd4274
Roy Chiu <rychiu13@gmail.com>
2018-08-30 13:50:36 -0500

    Add analyzer, basic setup and DB fetches

--- core/Analyzer.py
+++ core/Analyzer.py
@@ -1,15 +1,32 @@
 # TODO: Given list of champion matchups, count frequecy and other stats prior to analysis
-# TODO: Calculate win percentage in matchups and general win rates for champions
 # TODO: Build off win rate percentage vs different champions to imply counterpick
 # TODO: Get itemization if possible
 # TODO: Get runes if possible, calculate win rate of certain runes on champions
 # TODO: Calculate popularity of runes on champions
-from data import DivHarvester, MatchHarvester, SummHarvester
+from api.db.Database import Database
+from api.db.DB_Config import DBConfig
 
 
 class Analyzer:
 
     def __init__(self):
-        self.div_harvester = DivHarvester.DivHarvester()
-        self.summ_harvester = SummHarvester.SummHarvester()
-        self.match_harvester = MatchHarvester.MatchHarvester()
+        # Instantiate connection to DB for data and get default collection
+        self.db_config = DBConfig()
+        self.db_instance = Database()
+        self.database = self.db_instance.get_database(self.db_config.get_db_name())
+        self.match_collection = self.db_instance.get_collection(self.database, self.db_config.get_match_collection_name())
+
+    def get_total_matches(self):
+        """
+        Get total matches to be analyzed.
+        :return: num matches in db collection
+        """
+        return self.db_instance.get_num_documents(self.match_collection)
+
+    def get_matches(self):
+        """
+        Getter method to retrieve matches for stats.
+        :return: array with match details
+        """
+        return self.db_instance.get_bulk_documents(self.match_collection)
+

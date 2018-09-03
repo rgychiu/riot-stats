690800093bdf14ce28cddb5eec3916e770236b9d
Roy Chiu <rychiu13@gmail.com>
2018-09-03 16:52:33 -0500

    Implement champion winrate function

--- core/Analyzer.py
+++ core/Analyzer.py
@@ -33,11 +33,11 @@ class Analyzer:
         """
         return self.db_instance.get_bulk_documents(self.match_collection)
 
-    def get_matches(self, query):
+    def get_matches_by_query(self, query):
         """
         Overloaded method to get matches using specific query
-        :param query:
-        :return:
+        :param query: query to use to fetch matches from DB
+        :return: iterable with matched documents
         """
         return self.db_instance.get_bulk_documents(self.match_collection, query)
 
@@ -55,4 +55,27 @@ class Analyzer:
         :param champion_name: name of champion
         :return: id of champion
         """
-        return self.db_instance.get_single_document(self.champ_collection, {"name": champion_name})['id']
+        return self.db_instance.get_single_document(self.champ_collection, {"name": champion_name})['_id']
+
+    def champion_winrate(self, champion_name):
+        """
+        Calculate the champion win rate percentage from gathered matches.
+        :param champion_name: name of champion to calculate winrate for
+        :return: None if champion not found, float percentage otherwise
+        """
+        # Champion id returns as string
+        champion_id = int(self.get_champion_id(champion_name.capitalize()))
+        seen_matches = self.get_matches_by_query({"participants.championId": champion_id})
+        total_matches = seen_matches.count()
+        win_count = 0
+        for match in seen_matches:
+            participants = match['participants']
+            team_id = 0
+            for players in participants:
+                if players['championId'] == champion_id:
+                    team_id = players['teamId']
+            for team in match['teams']:
+                if team['teamId'] == team_id and team['win'] == 'Win':
+                    win_count += 1
+        return (win_count / total_matches) * 100
+

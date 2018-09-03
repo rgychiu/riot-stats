b9db4f0cf9cae9a2dd84b9da93d67dc2f501a43b
Roy Chiu <rychiu13@gmail.com>
2018-09-03 17:47:29 -0500

    Implement matchup win rate function

--- core/Analyzer.py
+++ core/Analyzer.py
@@ -57,20 +57,19 @@ class Analyzer:
         """
         return self.db_instance.get_single_document(self.champ_collection, {"name": champion_name})['_id']
 
-    def champion_winrate(self, champion_name):
+    def get_winrate(self, champion_id, cursor_matches):
         """
-        Calculate the champion win rate percentage from gathered matches.
-        :param champion_name: name of champion to calculate winrate for
-        :return: None if champion not found, float percentage otherwise
+        Helper method to get winrate of a champion from an iterable of match data.
+        :param champion_id: id of champion
+        :param cursor_matches: cursor object of match data
+        :return: winrate percentage
         """
-        # Champion id returns as string
-        champion_id = int(self.get_champion_id(champion_name.capitalize()))
-        seen_matches = self.get_matches_by_query({"participants.championId": champion_id})
-        total_matches = seen_matches.count()
+        total_matches = cursor_matches.count()
         win_count = 0
-        for match in seen_matches:
+        for match in cursor_matches:
             participants = match['participants']
             team_id = 0
+
             for players in participants:
                 if players['championId'] == champion_id:
                     team_id = players['teamId']
@@ -79,3 +78,28 @@ class Analyzer:
                     win_count += 1
         return (win_count / total_matches) * 100
 
+    def champion_winrate(self, champion_name):
+        """
+        Calculate the champion win rate percentage from gathered matches.
+        :param champion_name: name of champion to calculate winrate for
+        :return: None if champion not found, float percentage otherwise
+        """
+        # Champion id returns as string
+        champion_id = int(self.get_champion_id(champion_name.title()))
+        seen_matches = self.get_matches_by_query({"participants.championId": champion_id})
+        return self.get_winrate(champion_id, seen_matches)
+
+    def matchup_winrate(self, champion1_name, champion2_name):
+        """
+        Calculate the matchup win rate percentage from gathered matches.
+        :param champion1_name: first champ in matchup
+        :param champion2_name: second champ in matchup
+        :return: tuple of win percentages for matchup
+        """
+        champion1_id = int(self.get_champion_id(champion1_name.title()))
+        champion2_id = int(self.get_champion_id(champion2_name.title()))
+        seen_matches = self.get_matches_by_query({
+            "$and": [{"participants.championId": champion1_id}, {"participants.championId": champion2_id}]
+        })
+        champion1_winrate = self.get_winrate(champion1_id, seen_matches)
+        return champion1_winrate, 100 - champion1_winrate

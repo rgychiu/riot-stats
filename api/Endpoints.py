af5ac24f209d26ded6979bbf5198bb0adcc720a5
Roy Chiu <rychiu13@gmail.com>
2018-09-03 21:17:33 -0500

    Start adding endpoints

--- api/Endpoints.py
+++ api/Endpoints.py
@@ -1,1 +1,26 @@
 # TODO: Create endpoints for web app to be able to query analyzed data
+from analysis.ChampionAnalyzer import ChampionAnalyzer
+from analysis.MatchAnalyzer import MatchAnalyzer
+from flask import Flask, request
+from api.utils import *
+
+# Instantiate necessary classes
+app = Flask(__name__)
+champ_analyzer = ChampionAnalyzer()
+match_analyzer = MatchAnalyzer()
+
+# URL ENDPOINTS, CONSTANTS
+_MAX_LEN_RETURN = 10
+_GET_CHAMPION_WINRATE = '/winrate/<champion_name>'
+_GET_MATCHUP_WINRATE = '/winrate/matchup'
+
+# TODO: Catch errors
+@app.route(_GET_CHAMPION_WINRATE)
+def get_champion_winrate(champion_name):
+    return create_response(data=champ_analyzer.champion_winrate(champion_name), status=200)
+
+@app.route(_GET_MATCHUP_WINRATE)
+def get_matchup_winrate():
+    first_champ = request.args.get('champion_one')
+    second_champ = request.args.get('champion_two')
+    return create_response(data=match_analyzer.matchup_winrate(first_champ,second_champ), status=200)
---
+++ api/utils.py
@@ -0,0 +1,23 @@
+from flask import jsonify
+
+
+def create_response(data={}, status=200, message=''):
+    """
+    Wraps response in a consistent format throughout the API
+    Format inspired by https://medium.com/@shazow/how-i-design-json-api-responses-71900f00f2db
+
+    Modifications included:
+    - make success a boolean since there's only 2 values
+    - make message a single string since we will only use one message per response
+
+    IMPORTANT: data must be a dictionary where:
+    - the key is the name of the type of data
+    - the value is the data itself
+    """
+    response = {
+        'success': 200 <= status < 300,
+        'code': status,
+        'message': message,
+        'result': data
+    }
+    return jsonify(response), status
